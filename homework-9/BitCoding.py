a26cedc4033dde8f30e7e8356d09dd118e23f6b9
Victor Zadyabin <viktor.zadyabin@mail.ru>
2019-11-17 16:46:43 +0300

    its right^ tests are wrong

--- homework-9/BitCoding.py
+++ homework-9/BitCoding.py
@@ -15,33 +15,44 @@ def xehs(s):
     n = 0
     deg = 0
     for c in s[-1::-1]:
-        n += alphabet_64_10[c]*(64**deg)
+        n += alphabet_64_10[c] * (64 ** deg)
         deg += 1
     return n
 
+
 def binary_to_64(s):
     n_10 = 0
-    deg_10 = 0
+    deg = 0
     for c in s[-1::-1]:
-        n_10 += (2**deg_10)*int(c)
-        deg_10 += 1
+        n_10 += (2 ** deg) * int(c)
+        deg += 1
     return shex(n_10)
+
+
+def _64_to_binary(s):
+    n_10 = xehs(s)
+    n_2 = ""
+    while n_10 > 0:
+        n_2 += f"{n_10 % 2}"
+        n_10 //= 2
+    n_2 = n_2[::-1]
+    return n_2
 
 
 def additional_sort(f):
     i = 0
-    while i < len(f)-1:
+    while i < len(f) - 1:
         start = i
         finish = i
         for_sort = []
-        while i < len(f)-1 and f[i][1] == f[i+1][1]:
+        while i < len(f) - 1 and f[i][1] == f[i + 1][1]:
             i += 1
             finish = i
         if start != finish:
-            for_sort = f[start:finish+1]
+            for_sort = f[start:finish + 1]
             if len(for_sort):
                 for_replace = sorted(for_sort, key=operator.itemgetter(0), reverse=True)
-                for ind in range(start, finish+1):
+                for ind in range(start, finish + 1):
                     f[ind] = for_replace[ind - start]
                 i += 1
         else:
@@ -54,9 +65,15 @@ def form_dict_for_coding(s):
     coding = defaultdict(str)
     amount_of_1 = 0
     for c in s:
-        coding[c] = "1"*amount_of_1 + "0"
+        coding[c] = "1" * amount_of_1 + "0"
         amount_of_1 += 1
     return coding
+
+
+def form_dict_for_coding_reversed(s):
+    dic = form_dict_for_coding(s)
+    rev_dic = {i: j for i, j in zip(dic.values(), dic.keys())}
+    return rev_dic
 
 
 def encode(txt):
@@ -71,13 +88,23 @@ def encode(txt):
     return len(txt), string_sorted, encoded_string
 
 
-# def decode(length, chars, code):
-
-
-# print(alphabet_10_64)
-# print(alphabet_64_10)
+def decode(length, chars, code):
+    bin_n = _64_to_binary(code)
+    coding_dict = form_dict_for_coding_reversed(chars)
+    i = 0
+    r = ""
+    res = ""
+    while i < len(bin_n):
+        r += f"{bin_n[i]}"
+        if bin_n[i] == "0":
+            res += coding_dict[r]
+            r = ""
+        i += 1
+    return res
 
 print(xehs("BREAKFAST"))
 print(shex(10844745761445995))
 res = encode("ENGINEERING WITHOUT MANAGEMENT IS ART.")
 print(res)
+txt = decode(*res)
+print(txt)

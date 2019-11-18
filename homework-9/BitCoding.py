8eb6a2505dba863979ad9999d13b0c845365ad45
Victor Zadyabin <viktor.zadyabin@mail.ru>
2019-11-18 18:20:41 +0300

    i dont know what to do with this

--- homework-9/BitCoding.py
+++ homework-9/BitCoding.py
@@ -1,12 +1,13 @@
-from collections import Counter
+from collections import Counter, deque
 from math import ceil
 
 def shex(n):
-    bytes_l = []
+    bytes_l = deque()
     while n > 0:
-        bytes_l.append(32 + (n & 63))
+        bytes_l.appendleft(chr(32 + (n & 63)))
         n >>= 6
-    return bytes(reversed(bytes_l)).decode("ASCII")
+    return "".join(c for c in bytes_l)
+
 
 def xehs(s):
     n = 0
@@ -17,15 +18,16 @@ def xehs(s):
 
 
 def encode(txt):
-    s_frequency = list(sorted(list((j, i) for i, j in Counter(txt).most_common()), reverse=True))
-    coding_dict = dict(list((s_frequency[i][1], "1" * i + "0") for i in range(len(s_frequency))))
+    s_frequency = sorted(list((j, i) for i, j in Counter(txt).most_common()), reverse=True)
+    coding_dict = dict((s_frequency[i][1], "1" * i + "0") for i in range(len(s_frequency)))
     encoded_string_demo = "".join(coding_dict[i] for i in txt)
-    encoded_string_demo += "0" * (ceil(len(encoded_string_demo) / 6) * 6 - len(encoded_string_demo))
+    demo_len = len(encoded_string_demo)
+    encoded_string_demo += "0" * (ceil(demo_len / 6) * 6 - demo_len)
     return len(txt), "".join(coding_dict.keys()), shex(int(encoded_string_demo, 2))
 
 
 def decode(length, chars, code):
-    coding_dict = dict(list(("1" * i + "0", chars[i]) for i in range(len(chars))))
-    list_of_codes = (str(bin(xehs(code)))[2:] + "0").replace("0", "0 ").split()[:length]
+    coding_dict = dict(("1" * i + "0", chars[i]) for i in range(len(chars)))
+    list_of_codes = (bin(xehs(code))[2:] + "0").replace("0", "0 ").split()[:length]
     decoded_string = "".join(coding_dict[i] for i in list_of_codes)
     return decoded_string

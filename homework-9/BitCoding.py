c5bba9a125b94f239c225d8cd759b823493b74d7
Victor Zadyabin <viktor.zadyabin@mail.ru>
2019-11-19 09:39:30 +0300

    final

--- homework-9/BitCoding.py
+++ homework-9/BitCoding.py
@@ -1,20 +1,26 @@
 from collections import Counter, deque
 from math import ceil
 
+alf = {i: chr(i + 32) for i in range(64)}
+alf_rev = {chr(i + 32): i for i in range(64)}
+
 def shex(n):
     bytes_l = deque()
-    while n > 0:
-        bytes_l.appendleft(chr(32 + (n & 63)))
-        n >>= 6
+    demo_k = bin(n)[2:]
+    needed_zeroes = 1 if len(demo_k) % 6 != 0 else 0
+    k = (6 - len(demo_k) % 6)*needed_zeroes * "0" + demo_k
+    for i in range(0, len(k), 6):
+        bytes_l.append(alf[int(k[i:i+6], 2)])
     return "".join(c for c in bytes_l)
 
 
+
 def xehs(s):
-    n = 0
+    n = deque()
     for c in s:
-        digit = ord(c) - 32
-        n = n << 6 | digit
-    return n
+        digit = bin(alf_rev[c])[2:]
+        n.append(digit.rjust(6, "0"))
+    return int(str().join(i for i in n), 2)
 
 
 def encode(txt):
@@ -28,6 +34,6 @@ def encode(txt):
 
 def decode(length, chars, code):
     coding_dict = dict(("1" * i + "0", chars[i]) for i in range(len(chars)))
-    list_of_codes = (bin(xehs(code))[2:] + "0").replace("0", "0 ").split()[:length]
+    list_of_codes = (bin(xehs(code))[2:]).replace("0", "0 ").split()[:length]
     decoded_string = "".join(coding_dict[i] for i in list_of_codes)
     return decoded_string
